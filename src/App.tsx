49af8efc2e6f9f175896d9d122be30cf5b09aac5
Syed Safwan <91465866+safwan124@users.noreply.github.com>
2025-08-29 23:22:30 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from './Layout/Navbar'
+import Hero from './components/hero'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  )
+}
+
+export default App
